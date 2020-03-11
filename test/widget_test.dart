80137f9efece0a93f4be5a71adaf8c78eaf9925a
onikazu <programingmanagement@gmail.com>
2020-03-11 18:17:45 +0900

    completed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sqlite_practice/main.dart';
+import 'package:sqlite_practice/db_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
